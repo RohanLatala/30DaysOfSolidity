fc006605502092405795e5b5bfc14479ef34d1d3
Rohanlatala <rohanlatala4@gmail.com>
2025-08-31 10:26:21 +0530

    Day1: Initial commit

---
+++ Day1/src/Counter.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+contract Counter {
+    uint public count;
+
+    function increase()  external  {
+           count +=1;
+
+}
+
+function decrease() external {
+
+    count -=1;
+
+}
+
+}
+
+
